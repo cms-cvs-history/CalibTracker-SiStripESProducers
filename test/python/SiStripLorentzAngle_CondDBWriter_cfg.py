301c77b765008b3956f843be88db4ffb513cbdd9
cmsbuild <>
2009-04-01 17:01:28 +0000

    This commit was manufactured by cvs2git to create tag 'CalibTracker- SiStripESProducers-V03-01-08'.
    Sprout from master 2009-04-01 17:01:27 UTC sfrosali <sha1-2a4b5e7256594bc1ad6a8addcc6fd7dcfc992056@cern.ch> 'TID and TEC muh values equal to TIB/TOB values depending on module thickness. Muh values updated.'
    Cherrypick from master 2009-03-27 22:54:09 UTC Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch> 'correct strings':
        test/python/SiStripLorentzAngle_CondDBWriter_cfg.py

---
+++ test/python/SiStripLorentzAngle_CondDBWriter_cfg.py
@@ -0,0 +1,53 @@
+# The following comments couldn't be translated into the new config version:
+
+# upload to database 
+
+#string timetype = "timestamp"    
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Builder")
+
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring(''),
+    LorentzAngleBuilder = cms.untracked.PSet(
+        threshold = cms.untracked.string('INFO')
+    ),
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('INFO')
+    ),
+    destinations = cms.untracked.vstring('LorentzAngleBuilder.log')
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+process.source = cms.Source("EmptySource",
+    numberEventsInRun = cms.untracked.uint32(1),
+    firstRun = cms.untracked.uint32(1)
+)
+
+process.load("CalibTracker.SiStripESProducers.fake.SiStripLorentzAngleFakeESSource_cfi")
+process.load("CalibTracker.SiStripESProducers.DBWriter.SiStripLorentzAngleDummyDBWriter_cfi")
+
+process.PoolDBOutputService = cms.Service("PoolDBOutputService",
+    BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
+    DBParameters = cms.PSet(
+        messageLevel = cms.untracked.int32(2),
+        authenticationPath = cms.untracked.string('/afs/cern.ch/cms/DB/conddb')
+    ),
+    timetype = cms.untracked.string('runnumber'),
+    connect = cms.string('sqlite_file:LA_test_dummy.db'),
+    toPut = cms.VPSet(cms.PSet(
+        record = cms.string('SiStripLorentzAngleRcd'),
+        tag = cms.string('SiStripLorentzAngle_Ideal_31X')
+    ))
+)
+
+process.SiStripLorentzAngleGenerator.TIB_EstimatedValue = cms.double(0.0174)
+process.SiStripLorentzAngleGenerator.TOB_EstimatedValue = cms.double(0.0222)
+process.SiStripLorentzAngleGenerator.PerCent_Err = cms.double(20)
+
+process.p1 = cms.Path(process.siStripLorentzAngleDummyDBWriter)
+
+
